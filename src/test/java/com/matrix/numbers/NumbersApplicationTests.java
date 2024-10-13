f0f9389cdf3c10ec6516552f22a20f520d08ba50
Mykhailo Markhain <mmarkhain@totalwine.com>
2024-10-13 19:43:19 -0400

    init

---
+++ src/test/java/com/matrix/numbers/NumbersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matrix.numbers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumbersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
